67f14f03a516f0b6c2e50116a920ac9bada256bc
Ilya Krupin <dev@krupibox.ru>
2019-10-31 20:27:27 +0300

    Добавляет в серию images

--- gulpfile.js
+++ gulpfile.js
@@ -90,7 +90,6 @@ gulp.task('html', function () {
 gulp.task('copy', function () {
   return gulp.src([
     'source/fonts/**/*.{woff,woff2}',
-    'source/img/**',
     'source/js/**',
     'source//*.ico'
   ], {
@@ -103,5 +102,5 @@ gulp.task('clean', function () {
   return del('build');
 });
 
-gulp.task('build', gulp.series('clean', 'copy', 'css', 'sprite', 'html'));
+gulp.task('build', gulp.series('clean', 'copy', 'css', 'images', 'sprite', 'html'));
 gulp.task('start', gulp.series('build', 'server'));
