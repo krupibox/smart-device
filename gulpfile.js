4c5c667ee94b5ac0d82550aa45062059dc4483bd
Ilya Krupin <dev@krupibox.ru>
2019-11-07 16:05:50 +0300

    Добавляет маску к телефону

--- gulpfile.js
+++ gulpfile.js
@@ -42,6 +42,7 @@ gulp.task('server', function () {
   gulp.watch('source/sass/**/*.{scss,sass}', gulp.series('css'));
   gulp.watch('source/img/icon-*.svg', gulp.series('sprite', 'html', 'refresh'));
   gulp.watch('source/*.html', gulp.series('html', 'refresh'));
+  gulp.watch('source/js/*.js', gulp.series('minjs'));
 });
 
 gulp.task('refresh', function (done) {
@@ -80,7 +81,7 @@ gulp.task('sprite', function () {
       .pipe(gulp.dest('build/img'));
 });
 
-gulp.task("minjs", function () {
+gulp.task('minjs', function () {
   return gulp.src("source/js/*.js")
     .pipe(plumber())
     .pipe(gulp.dest("build/js"))
--- source/js/main.js
+++ source/js/main.js
@@ -3,55 +3,118 @@
 
   var body = document.querySelector('body');
   var header = body.querySelector('.inner');
-  var open = header.querySelector('.button--callback');
   var overlay = header.querySelector('.overlay');
   var modal = header.querySelector('.modal');
+  var buttonCallback = header.querySelector('.button');
   var close = modal.querySelector('img');
-  var form = header.querySelector('.modal__form');
-  var formName = form.querySelector('[name=user-callback-name]');
-  var formPhone = form.querySelector('[name=user-callback-phone]');
-  var formQuestion = form.querySelector('[name=user-callback-question]');
+  var modalForm = header.querySelector('.modal__form');
+  var form = body.querySelector('.form:not(.modal__form)');
+  var buttonModalForm = modalForm.querySelector('button');
+  var buttonForm = form.querySelector('button');
+  var modalFormName = modalForm.querySelector('#modal-form-name');
+  var modalFormPhone = modalForm.querySelector('#modal-form-phone');
+  var modalFormQuestions = modalForm.querySelector('#modal-form-questions');
+  var formName = form.querySelector('#form-name');
+  var formPhone = form.querySelector('#form-phone');
+  var formQuestions = form.querySelector('#form-questions');
+
+  var maskOptions = {
+    mask: '+{7} (000) 000-00-00'
+  };
+
+  var maskModalFromName = IMask(modalFormPhone, maskOptions);
+  var maskFormName = IMask(formPhone, maskOptions);
+
+  maskModalFromName.updateValue();
+  maskFormName.updateValue();
 
   var isStorageSupport = true;
 
   try {
-    formName.value = localStorage.getItem('Name');
+    modalFormName.value = localStorage.getItem('formName');
 
   } catch (err) {
     isStorageSupport = false;
   }
 
-  open.addEventListener('click', function (evt) {
+  var setItemForm = function () {
+    localStorage.setItem('formName', formName.value);
+    localStorage.setItem('formPhone', formPhone.value);
+    localStorage.setItem('formQuestions', formQuestions.value);
+  };
+
+  var setItemModalForm = function () {
+    localStorage.setItem('modalFormName', modalFormName.value);
+    localStorage.setItem('modalFormPhone', modalFormPhone.value);
+    localStorage.setItem('modalFormQuestions', modalFormQuestions.value);
+  };
+
+  var getItemForm = function () {
+    formName.value = localStorage.getItem('formName');
+    formPhone.value = localStorage.getItem('formPhone');
+    formQuestions.value = localStorage.getItem('formQuestions');
+  };
+
+  var getItemModalForm = function () {
+    modalFormName.value = localStorage.getItem('modalFormName');
+    modalFormPhone.value = localStorage.getItem('modalFormPhone');
+    modalFormQuestions.value = localStorage.getItem('modalFormQuestions');
+  };
+
+  var clearLocalStorage = function (forms) {
+    Array.from(forms.querySelectorAll('input:not([type=checkbox]), textarea')).
+    forEach(function (element) {
+      element.value = '';
+    });
+    window.localStorage.clear();
+    forms.reset();
+  };
+
+  if (isStorageSupport) {
+    getItemForm();
+    getItemModalForm();
+  }
+
+  buttonCallback.addEventListener('click', function (evt) {
     if (modal.classList.contains('modal--closed')) {
       evt.preventDefault();
       modal.classList.remove('modal--closed');
       body.classList.add('noscroll');
       overlay.style.display = 'block';
 
-      if (isStorageSupport) {
-        formName.value = localStorage.getItem('Name');
-        formPhone.value = localStorage.getItem('Phone');
-        formQuestion.value = localStorage.getItem('Question');
-      }
-
-      if (formName.value) {
-        formPhone.focus();
+      if (modalFormName.value) {
+        modalFormPhone.focus();
       } else {
-        formName.focus();
+        modalFormName.focus();
       }
 
-      if (formName.value && formPhone.value) {
-        formQuestion.focus();
+      if (modalFormName.value && modalFormPhone.value) {
+        modalFormQuestions.focus();
       }
+    }
+
+    modalForm.addEventListener('change', function () {
+      if (isStorageSupport) {
+        setItemModalForm();
+      }
+    });
+  });
+
+
+  buttonModalForm.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    clearLocalStorage(modalForm);
+  });
+
+  form.addEventListener('change', function () {
+    if (isStorageSupport) {
+      setItemForm();
     }
   });
 
-  form.addEventListener('submit', function () {
-    if (isStorageSupport) {
-      localStorage.setItem('Name', formName.value);
-      localStorage.setItem('Phone', formPhone.value);
-      localStorage.setItem('Question', formQuestion.value);
-    }
+  buttonForm.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    clearLocalStorage(form);
   });
 
   close.addEventListener('click', function (evt) {
